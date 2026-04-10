7b113bc18750e7f4588133938bd0306999858aba
VITOR MIGUEL RODRIGUES CEZARIO <25132565@dsn127m1241955.cfp127aluno.local>
2026-04-10 16:08:45 -0300

    Modificado estrutura de Props do DefaultButton com parametros Boolean para cada item a se utilizar do componente

--- code/frontend/family-sync-front-end/src/App.jsx
+++ code/frontend/family-sync-front-end/src/App.jsx
@@ -3,7 +3,27 @@ import DefaultButton from "./components/DefaultButton";
 function App() {
   return (
     <div>
-      <DefaultButton text="Login" horizontal="70px" vertical="16px"/>
+      <DefaultButton
+        text="Login"
+        horizontal="70px"
+        vertical="16px"
+        theme={true}
+      />
+      <DefaultButton
+        text="Cancelar"
+        horizontal="70px"
+        vertical="16px"
+        theme={false}
+        border={true}
+      />
+      <DefaultButton
+        text="Sair da Conta"
+        horizontal="50px"
+        vertical="0px"
+        theme={true}
+        border={true}
+        logout_image={true}
+      />
     </div>
   );
 }
