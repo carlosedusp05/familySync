cb442fe788892413132324e34edc43bf083ec099
VITOR MIGUEL RODRIGUES CEZARIO <25132565@dsn127m1241955.cfp127aluno.local>
2026-04-10 14:20:54 -0300

    Criado variáveis padrão

--- code/frontend/family-sync-front-end/src/components/DefaultButton.jsx
+++ code/frontend/family-sync-front-end/src/components/DefaultButton.jsx
@@ -1,7 +1,7 @@
 function DefaultButton(props) {
   return (
     <button
-      className="bg-[#FF8133] text-white rounded-[15px]
+      className="bg-orange text-white rounded-[15px]
         text-[20px] font-bold cursor-pointer
         duration-300 ease-out hover:-translate-y-0.5
         transition-all active:scale-90 active:brightness-90
