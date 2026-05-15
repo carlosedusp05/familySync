4e25316af5892be0329da345f967452c09c8a66e
DAVID LUCAS DOS SANTOS <25132827@dsn127m1246729.cfp127aluno.local>
2026-05-15 15:52:12 -0300

    Resolvendo Conflitos

--- code/frontend/family-sync-front-end/src/components/ui/ItemNotication.jsx
+++ code/frontend/family-sync-front-end/src/components/ui/ItemNotication.jsx
@@ -1,11 +1,11 @@
 function ItemNotication(props) {
   return (
-    <div className="w-full p-10 relative bg-yellow-cream rounded-[47px]">
-      <h2 className="text-orange-dark font-extrabold  text-3xl">
-        {props.title}
-      </h2>
-      <p className="w-full text-orange-dark text-2xl">{props.text}</p>
-      <span className="absolute text-orange-dark text-[16px] right-0 bottom-0 py-3 px-10">
+    <div className="w-[88%] p-10 relative bg-yellow-light rounded-2xl flex flex-col gap-2 transition-all duration-400 hover:scale-104">
+      <h2 className="text-terracota font-extrabold  text-2xl">{props.title}</h2>
+      <p className="w-full text-orange-dark text-xl font-medium">
+        {props.text}
+      </p>
+      <span className="absolute text-terracota right-0 bottom-0 py-3 px-10">
         {props.time}
       </span>
     </div>
