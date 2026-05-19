e10c7452c900e4e822972843326d31d44d7cc213
DavidLucas1122 <davidlucased@gmail.com>
2026-05-19 13:18:21 -0300

    Ajuste de estilo no calendário

--- code/frontend/family-sync-front-end/src/components/features/Calendar/CalendarView.jsx
+++ code/frontend/family-sync-front-end/src/components/features/Calendar/CalendarView.jsx
@@ -60,7 +60,7 @@ function CalendarView({
         {/* Div Eventos */}
         <div className="w-[50%] h-full flex flex-col p-30 gap-10  items-center">
           <h2 className="text-5xl text-white font-bold">Eventos Marcados</h2>
-          <div className="flex flex-col items-center gap-5 overflow-y-auto max-h-full w-[85%] px-2 ">
+          <div className="flex flex-col items-center gap-5 overflow-y-auto max-h-full h-full w-[85%] px-2 ">
             {dateEvent.length > 0 ? (
               <MultEventsField events={dateEvent} onEdit={handleOpenModal} />
             ) : (
