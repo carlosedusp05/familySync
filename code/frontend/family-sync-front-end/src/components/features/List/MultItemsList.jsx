e8839a929f421aa82e2e94c2d9133e0d1a559544
DAVID LUCAS DOS SANTOS <25132827@dsn127m1246729.cfp127aluno.local>
2026-06-09 16:59:51 -0300

    Ajustes

--- code/frontend/family-sync-front-end/src/components/features/List/MultItemsList.jsx
+++ code/frontend/family-sync-front-end/src/components/features/List/MultItemsList.jsx
@@ -30,8 +30,6 @@ function MultItemsList({
     setIsCreating(false);
   };
 
-  console.log(items_list);
-
   return (
     <div className="flex flex-col gap-2 h-[85%] overflow-y-auto px-2 [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-[#282828] [&::-webkit-scrollbar-thumb]:rounded-md">
       {items_list.map((item) => (
