edec9d925f63ffaa6feaeb871c68a59d8905fa36
Vfy-gif <vitormiguel.school@gmail.com>
2026-06-14 19:53:52 -0300

    retirando comentarios desnecessarios dos arquivos do hook

--- code/frontend/email_screens/convite-accept/app.js
+++ code/frontend/email_screens/convite-accept/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const token = urlParams.get("token");
+console.log(token);
+
+const url = `https://tcc-back-q3kw.onrender.com/v1/familysync/usuario-familia/emailEnviado?token='${token}'`;
+console.log(url);
+const response = await fetch(url, {
+  method: "POST", // ou 'PUT', dependendo do seu backend
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+console.log(response);
